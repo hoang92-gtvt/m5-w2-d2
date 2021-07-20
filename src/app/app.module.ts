839ebf9eacfd2a3302cd0c4c4512b942bc98ccc4
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-20 12:00:27 +0700

    truyen thuoc tinh cua componen cha cho component con

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import {FormsModule} from "@angular/forms";
+import { NameCardComponent } from './name-card/name-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    ArticleComponent,
+    NameCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/name-card/name-card.component.ts
@@ -0,0 +1,23 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-name-card',
+  templateUrl: './name-card.component.html',
+  styleUrls: ['./name-card.component.css']
+})
+export class NameCardComponent implements OnInit {
+  @Input()
+  cardName: String = '';
+
+  @Input()
+  email: String='';
+
+  @Input()
+  phone: String='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
